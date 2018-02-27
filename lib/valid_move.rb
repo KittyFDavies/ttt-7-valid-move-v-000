17ab70c1454e837b1ed7d7f501a60237216d143f
Kitty Davies <kittyfdavies@gmail.com>
2018-02-27 22:04:37 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
 def valid_move?(array, index_number)
   if
-    index_number.between(0, 8)
+    index_number.between?(0, 8)
       if position_taken?
         true
       else
